0d2d2501b3bd964bf539ecb4a1e8dc101dbada43
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2014-11-24 07:25:27 +0000

    N-grams

--- data_reader.py
+++ data_reader.py
@@ -9,28 +9,26 @@ class Bash_History(object):
 
 class Command(object):
   def __init__(self, line):
-    self.full_command = line
-    self.cmd = self.full_command.split(' ')[0]
-    self.params = self.full_command.split(' ')[1:]
+    self.full_cmd = line
+    self.cmd = self.full_cmd.split(' ')[0]
+    self.params = self.full_cmd.split(' ')[1:]
 
-# TODO
-# Add support for parsing multiple commands in a line. 
 # Important information if commands all listed in one line?
 def get_bash_histories():
   bash_histories = list()
   root_filepath = "./data/"
-  for r, d, f in os.walk(root_filepath):
-  files = f 
+  for r, d, files in os.walk(root_filepath):
     for f in files:
       with open(root_filepath + f, 'r') as bhf:
         bh = Bash_History()
-        for l.strip() in bhf:
-        # Check for multiple commands per line
+        for l in bhf:
+          l = l.strip()
+          # Check for multiple commands per line
           if ';' in l:
             cmds = l.split(';')
             for cmd in cmds:
               c = Command(l)
-                bh.add_commands(c)
+              bh.add_commands(c)
           else:
             c = Command(l)
             bh.add_commands(c)
@@ -41,4 +39,4 @@ def main():
   bash_histories = get_bash_histories()
 
 if __name__ == '__main__':
-main()
+  main()
--- stats.py
+++ stats.py
@@ -3,19 +3,40 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import data_reader
 
+def get_ngrams(cmds_list, n):
+  return zip(*[cmds_list[i:] for i in range(n)])
+
+def plot_ngram_cmds(bh):
+  cmd_ngrams = get_ngrams(bh.cmds)
+  ngram_counter = Counter(cmd_ngrams)
+  for s in ngram_counter:
+    ngram_counter[c] /= float(len(bh.cmds))
+
+  plt.bar(range(0, len(ngram_counter.keys())), ngram_counter.values())
+  plt.xlabel('Distinct Command Sequences')
+  plt.ylabel('Normalised Counts of Distinct Command Sequences')
+  plt.show()
+
+
 def plot_bash_history(bh):
   cmds = list()
   for c in bh.cmds:
     cmds.append(c.full_cmd)
 
-  plt.plot(cmds.values())
-  plt.xlabel('Distinct Commands')
-  plt.ylabel('Counts of Distinct Commands')
+  cmds_counter = Counter(cmds)
+  for c in cmds_counter:
+    cmds_counter[c] /= float(len(cmds))
+
+  plt.bar(range(0, len(cmds_counter.keys())), cmds_counter.values())
+  plt.xlabel('Distinct Command')
+  plt.ylabel('Normalised Counts of Distinct Command')
   plt.show()
 
 def main():
   bash_histories = data_reader.get_bash_histories()
-  plot_bash_history(bash_histories[0])
+  print len(bash_histories)
+  for bh in bash_histories:
+    plot_bash_history(bh)
 
 if __name__ == '__main__':
   main()
